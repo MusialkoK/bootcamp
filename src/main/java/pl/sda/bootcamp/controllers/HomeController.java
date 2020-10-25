3021f40aa6a64117ebf6bf74558ac012c882db51
Kamil <k.musialkowski@gmail.com>
2020-10-25 12:12:50 +0100

    course list

---
+++ src/main/java/pl/sda/bootcamp/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package pl.sda.bootcamp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        System.out.println("Hello world!");
+        return "home";
+    }
+}
