ffa83f18a6cbb6c8739893057af72c0b3d483f3a
Kamil <k.musialkowski@gmail.com>
2020-11-12 19:23:30 +0100

    home.html css added home.html navigation buttons added

--- src/main/java/pl/sda/bootcamp/controllers/HomeController.java
+++ src/main/java/pl/sda/bootcamp/controllers/HomeController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = "/")
+@RequestMapping
 public class HomeController {
 
-    @GetMapping
-    public String home(){
-        return "home";
-    }
+//    @GetMapping
+//    public String home(){
+//        return "home";
+//    }
 
     @PostMapping(value = "/action", params = "action=courses")
     public String goToCourseList(){
@@ -24,12 +24,22 @@ public class HomeController {
         return "redirect:admin/user/list";
     }
 
-//    @GetMapping
-//    public String home() {
-//        System.out.println("Hello world!");
-//        return "redirect:course/list";
-//    }
-//
+    @PostMapping(value = "/action", params = "action=add-course")
+    public String addCourse(){
+        return "redirect:course/add";
+    }
+
+    @PostMapping(value = "/action", params = "action=add-trainer")
+    public String addTrainer(){
+        return "redirect:user/add-trainer";
+    }
+
+    @GetMapping
+    public String home() {
+        System.out.println("Hello world!");
+        return "redirect:course/list";
+    }
+////
 //    @GetMapping("main-page")
 //    public String homePage() {
 //        System.out.println("Hello world!");
